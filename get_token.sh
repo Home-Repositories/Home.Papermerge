0184ebdc06e6b4489f7ddd14ae564217d49ba9c2
Pawel Wujczyk <pwujczyk@gmail.com>
2025-01-07 20:58:12 +0100

    get token

--- get_token.sh
+++ get_token.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-Type: application/json" -d '{"username":"john","password":"hohoho"}' http://localhost:12000/api/token
+CURL_OUT=$(curl -XPOST -H "Content-Type: application/json" -d '{"username":"john","password":"hohoho"}' http://localhost:12000/api/token)
+#TOKEN=$(echo $CURL_OUT | awk -F',:' '{print $1}')
+TOKEN=$(echo $CURL_OUT | jq -j '.access_token')
+echo "Token"
+echo $TOKEN
+echo "Token end"
+
+papermerge-cli --token $TOKEN --host "http://localhost:12000/" ls
+papermerge-cli --token $TOKEN --host "http://localhost:12000/" import /home/pawel/bin/Teczki/
