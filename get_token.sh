0858f9474f7519036b9af4c9027779e41882d8d8
Pawel Wujczyk <pwujczyk@gmail.com>
2025-01-08 23:27:17 +0100

    env variables

--- get_token.sh
+++ get_token.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-CURL_OUT=$(curl -XPOST -H "Content-Type: application/json" -d '{"username":"john","password":"hohoho"}' http://localhost:12000/api/token)
+export $(cat .env | xargs) && rails c
+USERNAME=$ENV_PAPERMERGE__AUTH__USERNAME
+PASSWORD=$ENV_PAPERMERGE__AUTH__PASSWORD
+echo "username:"
+echo $USERNAME
+echo $PASSWORD
+echo "endusername"
+CURL_OUT=$(curl -XPOST -H "Content-Type: application/json" -d "{\"username\":\"$USERNAME\",\"password\":\"$PASSWORD\"}" http://localhost:7020/api/token)
 #TOKEN=$(echo $CURL_OUT | awk -F',:' '{print $1}')
 TOKEN=$(echo $CURL_OUT | jq -j '.access_token')
 echo "Token"
 echo $TOKEN
 echo "Token end"
 
-papermerge-cli --token $TOKEN --host "http://localhost:12000/" me
-papermerge-cli --token $TOKEN --host "http://localhost:12000/" ls --parent-id=ae42169d-22c0-4074-bc6b-a872a7bb64fd
-papermerge-cli --token $TOKEN --host "http://localhost:12000/" import /home/pawel/bin/Teczki/
+papermerge-cli --token $TOKEN --host "http://localhost:7020/" me
+papermerge-cli --token $TOKEN --host "http://localhost:7020/" ls --parent-id=ae42169d-22c0-4074-bc6b-a872a7bb64fd
+#papermerge-cli --token $TOKEN --host "http://localhost:12000/" import /home/pawel/bin/Teczki/
